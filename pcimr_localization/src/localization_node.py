e7df2df7123fe2c52081011d22bca3ce31d55ba2
unstablecursor <recepdeniza@gmail.com>
2020-11-30 21:05:30 +0100

    create skeleton of node

--- pcimr_localization/src/localization_node.py
+++ pcimr_localization/src/localization_node.py
@@ -8,7 +8,7 @@ from threading import Lock
 from std_msgs.msg import String
 from sensor_msgs.msg import LaserScan
 from geometry_msgs.msg import Point, Twist
-from nav_msgs.msg import Odometry, OccupancyGrid
+from nav_msgs.msg import Odometry, OccupancyGrid, MapMetaData
 from visualization_msgs.msg import Marker
 
 class LocalizationNode:
@@ -21,57 +21,53 @@ class LocalizationNode:
         self.map_sub = rospy.Subscriber('/map', OccupancyGrid, self.get_map)
         self.sub_scan_sub = rospy.Subscriber('/scan', LaserScan, self.get_scan)
 
-        # self.max_velocity = rospy.get_param('/controller_node/max_velocity', 0.5)
-        # self.stop_distance = rospy.get_param('/controller_node/stop_distance', 0.2)
-        # self.slowdown_distance = rospy.get_param('/controller_node/slowdown_distance', 0.4)
-        # self.robot_ns = rospy.get_param('/controller_node/robot_namespace', "rto-1")
-        self.map_updates_pub = rospy.Publisher('/map_updates', OccupancyGrid, queue_size=10)
-        self.pub_twist_pub = rospy.Publisher('/visualization/robot_pos', Marker, queue_size=10)
+        self.robot_pos_pub = rospy.Publisher('/robot_pos', Point, queue_size=10)
+        self.robot_pos_viz_pub = rospy.Publisher('/visualization/robot_pos', Marker, queue_size=10)
         self.robot_ros_map_pub  = rospy.Publisher('/robot_pos_map', OccupancyGrid, queue_size=10)
 
         self.robot_move = String()
         self.map = OccupancyGrid()
-             
         self.scan = LaserScan()
-        self.robot_twist = Twist()
-        self.filtered_twist = Twist()
+
+        self.robot_pos = Point()
+        self.robot_pos_viz = Marker()
+        self.robot_pos_map = OccupancyGrid()
 
     def get_move(self, msg):
-        self.robot_twist = msg
+        self.robot_move = msg
 
     def get_map(self, msg):
-        self.robot_twist = msg
+        self.map = msg
 
-    def get_scan(self, msg):
-        self.robot_twist = msg
-
-    
-    def filter_twist(self):
-        rospy.loginfo(f"Got twist: {self.robot_twist}")
-        self.filtered_twist = self.robot_twist
-        if self.robot_twist.linear.x > self.max_velocity:
-            self.filtered_twist.linear.x = self.max_velocity
-        if self.robot_twist.linear.x > 0.0 :
-            min_dist_obs = min(self.scan.ranges)
-            if min_dist_obs < self.slowdown_distance:
-                val = min_dist_obs - self.stop_distance
-                rospy.loginfo(f"Val : {val}, diif {self.slowdown_distance - self.stop_distance} ")
-                if val > 0.0:
-                    self.filtered_twist.linear.x = self.filtered_twist.linear.x * (val / (self.slowdown_distance - self.stop_distance)) 
-                else: 
-                    self.filtered_twist.linear.x = 0.0
-        else: 
-            self.filtered_twist.linear.x = 0.0
-        rospy.loginfo(f"Filtered twist: {self.filtered_twist}")
-    
     def get_scan(self, msg):
         self.scan = msg
 
+    def map_to_np(self, map: OccupancyGrid):
+        np_map = np.transpose(np.asarray(map.data, dtype=np.int8).reshape(map.info.width, map.info.height))
+        return np_map
+
+    def np_to_map(self, arr: np.array):
+        arr = np.array(np.transpose(arr), dtype=np.int8)
+        grid = OccupancyGrid()
+        grid.data = arr.ravel()
+        grid.info = MapMetaData()
+        grid.info.height = arr.shape[0]
+        grid.info.width = arr.shape[1]
+
+        return grid
+    
+    def filter_map(self):
+        np_map = self.map_to_np(self.map)
+        # TODO: Filter map here...
+        self.robot_pos_map = self.np_to_map(np_map)
+        
+
     def run(self, rate: float = 1):
         while not rospy.is_shutdown():
-            self.filter_twist()
-            self.pub_twist.publish(self.filtered_twist)
-            #print(f"Published move \n {self.filtered_twist}")
+            self.filter_map()
+            self.robot_pos_pub.publish(self.robot_pos)
+            self.robot_pos_viz_pub.publish(self.robot_pos_viz)
+            self.robot_ros_map_pub.publish(self.robot_pos_map)
 
             if rate:
                 rospy.sleep(1/rate)
@@ -80,5 +76,5 @@ class LocalizationNode:
 if __name__ == "__main__":
     rospy.init_node('localization_node')
 
-    simple_sim_node = LocalizationNode()
-    simple_sim_node.run(rate=20)
+    node = LocalizationNode()
+    node.run(rate=10)
